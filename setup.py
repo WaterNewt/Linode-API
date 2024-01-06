de9ed1cf17c4f15df9c3f5d4e0b725eaf2d61bcd
WaterNewt <134024849+WaterNewt@users.noreply.github.com>
2024-01-06 18:10:33 +0500

    Added more info.

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='mylinode_api',
     description="A module to simplify Linode interactions.",
+    long_description=open("README.md", "r").read(),
+    author="Yunus Ruzmetov",
     author_email="waternewtinfo@gmail.com",
+    url="https://github.com/WaterNewt/Linode-API",
+    download_url="https://github.com/WaterNewt/Linode-API/releases/tag/v1.0.0",
     version='1.0.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
