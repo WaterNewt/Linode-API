2ae1d40f611b515c9b0f5ccb44f87431bc5ddcf9
WaterNewt <134024849+WaterNewt@users.noreply.github.com>
2024-01-06 17:15:40 +0500

    Changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mylinode-api',
+    name='mylinode_api',
     description="A module to simplify Linode interactions.",
     author_email="waternewtinfo@gmail.com",
     version='1.0.0',
