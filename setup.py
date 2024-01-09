fc4d5609725be6f8e4058cc5144b96e9c05addbe
WaterNewt <134024849+WaterNewt@users.noreply.github.com>
2024-01-09 11:27:21 +0500

    Improved setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mylinode_api',
+    version='1.0.0',
     description="A module to simplify Linode interactions.",
     long_description=open("README.md", "r").read(),
     author="Yunus Ruzmetov",
@@ -9,8 +10,20 @@ setup(
     url="https://github.com/WaterNewt/Linode-API",
     download_url="https://github.com/WaterNewt/Linode-API/releases/tag/v1.0.0",
     license='MIT',
-    version='1.0.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=['requests'],
+    python_requires='>=3.6',  # Specify the minimum Python version
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords=['Linode API interaction', 'API', 'cloud', 'Linode'],
+    include_package_data=True,
 )
